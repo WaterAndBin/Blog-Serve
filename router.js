8cc47f4dca2782478246b9f679342ce17db2d0f9
weibin.ye <weibin.ye@ly.com>
2024-03-01 18:23:48 +0800

    3/1

--- router.js
+++ router.js
@@ -4,4 +4,5 @@ module.exports = (app) => {
     app.use('/role', require('./router/role'))
     app.use('/tabs', require('./router/tabs'))
     app.use('/menu', require('./router/menu'))
+    app.use('/permissions', require('./router/permissions'))
 }
